62c38bfce46995aab32b74b9cf18ccdc049adc6e
dalong <dalong0514@126.com>
2023-08-26 06:23:17 +0800

    change model

--- audio_to_text.py
+++ audio_to_text.py
@@ -6,7 +6,7 @@ from modelscope.utils.constant import Tasks
 def audio_to_text():
     inference_pipeline = pipeline(
         task=Tasks.auto_speech_recognition,
-        model='damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+        model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
         output_dir='./output_dir')
 
     rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/AI-1.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693035545&Signature=jEFMRtIQ3WTzLjC42S%2FQmroXwnM%3D')
