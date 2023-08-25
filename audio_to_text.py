afec60c74950e0bb85dc4c3ec4966a0039be0d21
dalong <dalong0514@126.com>
2023-08-26 00:22:21 +0800

    test audio to text

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,10 +1,19 @@
+import os, time
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 
-inference_pipeline = pipeline(
-    task=Tasks.auto_speech_recognition,
-    model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
-    model_revision="v1.2.4")
 
-rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/AI-1.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693016135&Signature=jHule%2BzsbTfzX17It3V%2BjDNAsg4%3D')
-print(rec_result)
+def audio_to_text():
+    inference_pipeline = pipeline(
+        task=Tasks.auto_speech_recognition,
+        model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+        output_dir='./output_dir')
+
+    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/AI-1.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693016135&Signature=jHule%2BzsbTfzX17It3V%2BjDNAsg4%3D')
+
+if __name__ == '__main__':
+    start_time = time.time()
+    audio_to_text()
+    end_time = time.time()
+    print('OK!')
+    print('Time Used: ' + str(end_time - start_time) + 's')
