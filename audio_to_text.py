bd7c5ebc1a822f0dd832966acb702690fefb1c56
dalong <dalong0514@126.com>
2023-08-26 00:16:21 +0800

    test audio to text

--- audio_to_text.py
+++ audio_to_text.py
@@ -6,5 +6,5 @@ inference_pipeline = pipeline(
     model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
     model_revision="v1.2.4")
 
-rec_result = inference_pipeline(audio_in='/audio-to-text/AI-1.wav')
+rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/AI-1.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693016135&Signature=jHule%2BzsbTfzX17It3V%2BjDNAsg4%3D')
 print(rec_result)
