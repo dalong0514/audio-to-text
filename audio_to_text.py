b5fa001c7f924de46b32489b21cc7bc9ce2f58bb
dalong <dalong0514@126.com>
2023-08-26 11:54:45 +0800

    upload audio

--- audio_to_text.py
+++ audio_to_text.py
@@ -6,11 +6,10 @@ from modelscope.utils.constant import Tasks
 def audio_to_text():
     inference_pipeline = pipeline(
         task=Tasks.auto_speech_recognition,
-        # model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
-        model='damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+        model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
         output_dir='./output_dir')
 
-    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/AI-1.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693035545&Signature=jEFMRtIQ3WTzLjC42S%2FQmroXwnM%3D')
+    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/CH0101AI%E6%97%B6%E4%BB%A3%E7%9A%84%E9%98%85%E8%AF%BB.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1693058050&Signature=KFAHTFUMi994a1wAdWUl2SZ%2FPv4%3D')
 
 if __name__ == '__main__':
     start_time = time.time()
