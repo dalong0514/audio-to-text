86391d1feb7c9e34f50fca961670e04f3de60462
dalong <dalong0514@126.com>
2023-10-15 15:30:26 +0800

    upload audio

--- audio_to_text.py
+++ audio_to_text.py
@@ -9,7 +9,7 @@ def audio_to_text():
         model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
         output_dir='./output_dir')
 
-    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20190712%E5%BC%A0%E6%B6%9B%E5%AE%A3%E8%B4%AF%E7%9F%B3%E5%8C%96%E8%A7%842.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1697196461&Signature=WP%2FEFrpDqtcfZ2e5H5CVT9BBLXA%3D')
+    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20231015%E9%98%B3%E5%BF%97%E5%B9%B3AI%E9%97%AD%E9%97%A8%E5%88%86%E4%BA%AB%E4%BC%9A01.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1697368453&Signature=bSiaJb8wsMTOn38cINE2E4nL6ds%3D')
 
 if __name__ == '__main__':
     start_time = time.time()
