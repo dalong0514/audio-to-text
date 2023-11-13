558f5bf116eacae842de8ea5ab12c38575251aa9
dalong <dalong0514@126.com>
2023-11-13 20:43:37 +0800

    20230731M2-Ultra-AI大模型的新希望

--- audio_to_text.py
+++ audio_to_text.py
@@ -9,7 +9,7 @@ def audio_to_text():
         model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
         output_dir='./output_dir')
 
-    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20231110%E4%B8%8E%E6%9D%8E%E5%A5%95%E9%93%AE%E4%BA%A4%E6%B5%81%E5%BB%BA%E7%AD%91%E7%94%B2%E7%B1%BB%E4%BB%93%E5%BA%93%E9%9C%80%E6%B1%82.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1699891099&Signature=x%2BG%2FoM3ijc7fRbDyBv2Kz62DqHY%3D')
+    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20230731M2-Ultra-AI%E5%A4%A7%E6%A8%A1%E5%9E%8B%E7%9A%84%E6%96%B0%E5%B8%8C%E6%9C%9B.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1699892994&Signature=cxjzvFS7TmpMB7Gk86yYkqC4w3s%3D')
 
 if __name__ == '__main__':
     start_time = time.time()
