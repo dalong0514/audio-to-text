9868f94651d878f7b987a0bdec8f838bd253487c
dalong <dalong0514@126.com>
2023-11-26 22:54:45 +0800

    20230705舒伟杰培训HAZOP

--- audio_to_text.py
+++ audio_to_text.py
@@ -9,7 +9,7 @@ def audio_to_text():
         model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
         output_dir='./output_dir')
 
-    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20231110%E5%85%AC%E5%8F%B8%E7%BB%8F%E8%90%A5%E4%BC%9A%E8%AE%AE-%E4%B8%87%E6%80%BB.mp3?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1699985659&Signature=HYe99wKgoVHAA2JYOmsj4QwKVpE%3D')
+    rec_result = inference_pipeline(audio_in='https://forgpt.oss-cn-hangzhou.aliyuncs.com/20230705%E8%88%92%E4%BC%9F%E6%9D%B0%E5%9F%B9%E8%AE%ADHAZOP.m4a?OSSAccessKeyId=LTAI4GHKoH2Z5txaWP3NpNTx&Expires=1701023897&Signature=1VvihORJaA4wk%2FUmw9FQ83N9WBM%3D')
 
 if __name__ == '__main__':
     start_time = time.time()
